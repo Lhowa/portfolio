cd673088b3d03f6477d39e973b98c29e93c3ebe8
Landon Howa <landonhowa@landons-mbp.lan>
2020-05-22 14:34:11 -0600

    styled projects and skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
+let skillsObjectsArray = [
+    {title: 'Skill 1', imagePath: 'image 1'},
+    {title: 'Skill 2', imagePath: 'image 2'},
+    {title: 'Skill 3', imagePath: 'image 3'},
+    {title: 'Skill 4', imagePath: 'image 4'},
+    {title: 'Skill 5', imagePath: 'image 5'},
+    {title: 'Skill 6', imagePath: 'image 6'},
+    {title: 'Skill 7', imagePath: 'image 7'},
+    {title: 'Skill 8', imagePath: 'image 8'}
+]
+
 const Skills = () => {
     return(
         <div className='skills-section' id='skills'>
-            <ul>
-                <li>Some Skills</li>
-                <li>Some More Skills</li>
-            </ul>
+            <h2>Skills</h2>
+                <div className='skillsContainer'>
+                    {skillsObjectsArray.map(skill =>
+                        (<div key={skill.title} className='individualSkillContainer'>
+                            <p>{skill.imagePath}</p>
+                            <h3>{skill.title}</h3>
+                        </div>)
+                    )}
+                </div>
         </div>
     )
 }
