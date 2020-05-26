ef77d9e70fba362258d767e9ea587f8555eb7181
lhowa <howa.landon@gmail.com>
2020-05-26 15:37:46 -0600

    Adjusted desktop styles

--- src/components/about.js
+++ src/components/about.js
@@ -10,9 +10,9 @@ const About = () => {
             </div>
             <p>
                 Hello there! I'm Landon, a software engineer based in South Jordan, UT. My engineering background is
-                mainly comprised of Java and Javascript among various other technologies. I have an Associate's 
+                comprised mostly of Java and Javascript among various other technologies. I have an Associate's 
                 degree in Computer Science and Information Systems with Honors from Salt Lake Community College. 
-                My studies mainly included software engineering, web development, and networking.
+                My studies included software engineering, web development, and networking.
             </p>
         </div>
     )
